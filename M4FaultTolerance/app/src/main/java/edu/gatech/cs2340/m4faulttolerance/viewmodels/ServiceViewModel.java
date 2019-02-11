3f05f66270564341fba9378f45d8156750a7aea9
Peggy_Cao <yuqi.cao.sjtu@gmail.com>
2019-02-11 15:50:56 -0500

    M4 finished

--- M4FaultTolerance/app/src/main/java/edu/gatech/cs2340/m4faulttolerance/viewmodels/ServiceViewModel.java
+++ M4FaultTolerance/app/src/main/java/edu/gatech/cs2340/m4faulttolerance/viewmodels/ServiceViewModel.java
@@ -11,6 +11,16 @@ import edu.gatech.cs2340.m4faulttolerance.services.NormalService;
 import edu.gatech.cs2340.m4faulttolerance.services.RetryService;
 import edu.gatech.cs2340.m4faulttolerance.services.SimulatedService;
 import edu.gatech.cs2340.m4faulttolerance.services.SlowService;
+//import io.github.jolly.*;
+import io.github.jolly.circuitbreaker.CircuitBreakerPolicy;
+import io.github.jolly.circuitbreaker.CircuitBreakerPolicyBuilder;
+import io.github.jolly.timeout.TimeoutPolicy;
+import io.github.jolly.timeout.TimeoutPolicyBuilder;
+import io.github.jolly.fallback.FallbackPolicy;
+import io.github.jolly.fallback.FallbackPolicyBuilder;
+import io.github.jolly.retry.RetryPolicy;
+import io.github.jolly.retry.RetryPolicyBuilder;
+
 
 
 /**
@@ -37,21 +47,58 @@ public class ServiceViewModel extends AndroidViewModel {
         SimulatedService normalService = new NormalService(request);
         SimulatedService badService = new BadService(request);
 
+        CircuitBreakerPolicy<String> pol = new CircuitBreakerPolicyBuilder<>()
+                .rateThreshold(100)
+                .sizeRingBufferHalfOpen(2)
+                .sizeRingBufferClosed(4)
+                .build();
+
+
         try {
-            badService.executeTheService();
-            badService.executeTheService();
-            badService.executeTheService();
-            return normalService.executeTheService();
+            //badService.executeTheService();
+            pol.exec(badService::executeTheService);
+            //badService.executeTheService();
+            //badService.executeTheService();
+//            return normalService.executeTheService();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+
+        try {
+            //badService.executeTheService();
+            pol.exec(badService::executeTheService);
+//            badService.executeTheService();
+//            badService.executeTheService();
+//            return normalService.executeTheService();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            //badService.executeTheService();
+            pol.exec(badService::executeTheService);
+//            badService.executeTheService();
+//            badService.executeTheService();
+//            return normalService.executeTheService();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+        return normalService.executeTheService();
     }
 
     public String makeBadRequest(String request) {
         SimulatedService service = new BadService(request);
+        SimulatedService normalService = new NormalService(request);
+        FallbackPolicy<String> pol = new FallbackPolicyBuilder<>(normalService::executeTheService)
+                .build();
+
         try {
-            return service.executeTheService();
+
+            String res = pol.exec(service::executeTheService);
+            return res;
+            //return service.executeTheService();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -60,18 +107,36 @@ public class ServiceViewModel extends AndroidViewModel {
 
     public String makeSlowRequest(String request) {
         SimulatedService service = new SlowService(request);
+
+        TimeoutPolicy pol = new TimeoutPolicyBuilder()
+                .duration(1000)
+                .cancelFuture(false)
+                .build();
+
+
         try {
-            return service.executeTheService();
+            pol.exec(service::executeTheService);
+
+            //return result;
+            return "Timeout Happened";
         } catch (Exception e) {
+
             e.printStackTrace();
+            //return "Timeout happened";
         }
         return "Bad Result";
     }
 
     public String makeRetryRequest(String request) {
         SimulatedService service = new RetryService(request);
+        RetryPolicy pol = new RetryPolicyBuilder()
+                .attempts(4)
+                .waitDuration(500)
+                .build();
+
         try {
-            return service.executeTheService();
+            return (String)pol.exec(service::executeTheService);
+            //return service.executeTheService();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
